1ab655a95358cc9798598708cc4876d482ecab4b
Cash Williams <CashWilliams@gmail.com>
2017-05-25 14:35:04 -0400

    background drush runserver

--- tests/haas/runserver.sh
+++ tests/haas/runserver.sh
@@ -6,7 +6,4 @@ blt ci:setup
 
 echo "Starting Drupal server... "
 cd docroot
-drush runserver `hostname`:8888
-
-# echo "Scanning with arachni..."
-# ./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:0:15
+drush runserver `hostname`:8888 &
