fc0f1c6e3e4c50e5ecc79beccc9a8567533b0be0
Mariano Asselborn <marianoasselborn@gmail.com>
2017-05-25 10:02:33 -0400

    blegh

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Mock loopback interface..."
-cat /etc/hosts
-hostname
-
 echo "Scanning with arachni..."
-# ./tests/haas/arachni/bin/arachni http://127.0.0.1:8888
+./tests/haas/arachni/bin/arachni http://${hostname}:8888
