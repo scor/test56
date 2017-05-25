b55e2ee7d82177b1a767f74733fb79b4ea824923
Cash Williams <CashWilliams@gmail.com>
2017-05-25 16:35:37 -0400

    fixing typos

--- tests/haas/stop_arachni.sh
+++ tests/haas/stop_arachni.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 HOSTNAME=`hostname`
-ADDRESS=`cat /etc/hosts | grep $HOSTNAME | cut -d "	" -f1 
+ADDRESS=`cat /etc/hosts | grep $HOSTNAME | cut -d "	" -f1` 
 
 echo "Stopping arachni proxy..."
 wget -e use_proxy=yes -e http_proxy=${$ADDRESS}:9595 http://arachni.proxy/shutdown
