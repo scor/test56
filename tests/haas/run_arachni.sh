da17720237151743018cb8aa7baa247d2f006871
Stacy Watts <stacy.watts@acquia.com>
2017-05-25 11:50:44 -0400

    try my ip right os

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Scanning with arachni..."
- ./tests/haas/arachni/bin/arachni http://`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`:8888 --timeout 0:0:15
+ ./tests/haas/arachni/bin/arachni http://`ip a |grep "inet " | grep -v "127.0.0.1" |cut -d "/" -f 1`:8888 --timeout 0:0:15
 #curl http://127.0.0.1:8888
