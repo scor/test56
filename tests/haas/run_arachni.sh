109db4ce17cb961e14bb2bea11b46e416ad04cb1
Mariano Asselborn <marianoasselborn@gmail.com>
2017-05-25 10:10:25 -0400

    sfsdzg

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Scanning with arachni..."
-./tests/haas/arachni/bin/arachni http://${hostname}:8888
+./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:0:15
