ab480447186813b595003b5ebe6ea1306a74cbf6
Cash Williams <CashWilliams@gmail.com>
2017-05-25 17:54:38 -0400

    echo polling

--- tests/haas/poll_arachni.sh
+++ tests/haas/poll_arachni.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+
+echo "Polling for arachni..."
 while [ `pgrep arachni` ]
 do
   sleep 10
+  echo "Waiting 10 seconds..."
 done
 
+echo "Arachni completed..."
+
+echo "Killing drush server..."
 pgrep drush | xargs kill 2>/dev/null
