0a483476a719bcdd4ce067648462fd1acc77adde
Cash Williams <CashWilliams@gmail.com>
2017-05-25 14:21:59 -0400

    Testing full round trip

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Scanning with arachni..."
- ./tests/haas/arachni/bin/arachni http://`ip addr |grep "inet " | grep -v "127.0.0.1" |cut -d "/" -f 1 | tr -s ' ' | cut -d ' ' -f 3`:8888 --timeout 0:0:15
-#curl http://127.0.0.1:8888
+ ./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:2:00 --checks=*,-file_inclusion,-ldap_injection,-no_sql_injection*,-path_traversal,-backdoors,-backup_directories,-backup_files,-common_admin_interfaces,-common_directories,-common_files,-directory_listing,-captcha,-cvs_svn_users,-emails,-private_ip,-ssn,-localstart_asp,-webdav --scope-exclude-pattern '.*\.css' --scope-exclude-pattern '\/devel\/.*' --scope-exclude-pattern '\/sites\/.*\/files\/.*' --scope-exclude-pattern 'user\/logout' --scope-auto-redundant=10 --audit-links --audit-forms --platforms 'linux,sql,mysql,apache,php,symfony'
