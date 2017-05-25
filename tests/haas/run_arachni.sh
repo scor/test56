f605c232209b7a644eb2b4ea1e889f1010d95f3f
Stacy Watts <stacy.watts@acquia.com>
2017-05-25 12:07:11 -0400

    try my ip not with just ip

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Scanning with arachni..."
- ./tests/haas/arachni/bin/arachni http://`ip a |grep "inet " | grep -v "127.0.0.1" |cut -d "/" -f 1`:8888 --timeout 0:0:15
+ ./tests/haas/arachni/bin/arachni http://`ip addr |grep "inet " | grep -v "127.0.0.1" |cut -d "/" -f 1 | tr -s ' ' | cut -d ' ' -f 3`:8888 --timeout 0:0:15
 #curl http://127.0.0.1:8888
