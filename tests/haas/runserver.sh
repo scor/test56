cc0f198a3e708a3e310410d1f58bab806d5ad9c6
Stephane Corlosquet <scorlosquet@gmail.com>
2017-05-25 19:16:04 -0400

    Generate list of URLs (#5)

--- tests/haas/runserver.sh
+++ tests/haas/runserver.sh
@@ -6,4 +6,15 @@ blt ci:setup
 
 echo "Starting Drupal server... "
 cd docroot
+pwd
+cd modules
+git clone git@github.com:CashWilliams/drupen.git
+cd ..
+
 drush runserver `hostname`:8888 &
+
+drush user-create demo --password="demo" --mail="demo@example.com"
+drush en -y drupen
+drush --uri=`hostname`:8888 route-list > /mnt/tmp/local.prod/source/tests/haas/drupal_route_list.txt
+head -n 30 /mnt/tmp/local.prod/source/tests/haas/drupal_route_list.txt > /mnt/tmp/local.prod/source/tests/haas/drupal_route_list_lite.txt
+cat /mnt/tmp/local.prod/source/tests/haas/drupal_route_list_lite.txt
