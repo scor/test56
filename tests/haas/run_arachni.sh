44a9564d673a5080260e4c53c7577a2ae1dc092b
Cash Williams <CashWilliams@gmail.com>
2017-05-25 16:10:21 -0400

    ahhhhhhhh

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 HOSTNAME=`hostname`
-ADDRESS=`cat /etc/hosts | grep $HOSTNAME | cut -d "	" -f1 
+ADDRESS=`cat /etc/hosts | grep $HOSTNAME | cut -d "	" -f1`
 echo "Binding proxy on ${$ADDRESS}"
 
 echo "Scanning with arachni..."
- ./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:2:00 --checks=*,-file_inclusion,-ldap_injection,-no_sql_injection*,-path_traversal,-backdoors,-backup_directories,-backup_files,-common_admin_interfaces,-common_directories,-common_files,-directory_listing,-captcha,-cvs_svn_users,-emails,-private_ip,-ssn,-localstart_asp,-webdav --scope-exclude-pattern '.*\.css' --scope-exclude-pattern 'user\/login' --scope-exclude-pattern '\/devel\/.*' --scope-exclude-pattern '\/sites\/.*\/files\/.*' --scope-exclude-pattern 'user\/logout' --scope-auto-redundant=10 --audit-links --audit-forms --platforms 'linux,sql,mysql,apache,php,symfony' --plugin 'proxy:port=9595,bind_address=${$ADDRESS}'
+ ./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:2:00 --checks=*,-file_inclusion,-ldap_injection,-no_sql_injection*,-path_traversal,-backdoors,-backup_directories,-backup_files,-common_admin_interfaces,-common_directories,-common_files,-directory_listing,-captcha,-cvs_svn_users,-emails,-private_ip,-ssn,-localstart_asp,-webdav --scope-exclude-pattern '.*\.css' --scope-exclude-pattern 'user\/login' --scope-exclude-pattern '\/devel\/.*' --scope-exclude-pattern '\/sites\/.*\/files\/.*' --scope-exclude-pattern 'user\/logout' --scope-auto-redundant=10 --audit-links --audit-forms --platforms 'linux,sql,mysql,apache,php,symfony' --plugin 'proxy:port=9595,bind_address=${$ADDRESS}' &
