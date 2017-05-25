7fecdf4913e497d2ed15b0bd54c40b16a9de9367
Mariano Asselborn <marianoasselborn@gmail.com>
2017-05-25 09:45:31 -0400

    blah

--- install_arachni.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget -qO- https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-linux-x86_64.tar.gz | tar xvz -C vendor/
-mv vendor/arachni* vendor/arachni
-
-export PATH=${pwd}/vendor/arachni:$PATH
