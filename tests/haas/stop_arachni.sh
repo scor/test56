9b5c5ea06271eb01e1a250eecbc1512fa7f6f3b5
Cash Williams <CashWilliams@gmail.com>
2017-05-25 16:55:26 -0400

    still fixing this thing

--- tests/haas/stop_arachni.sh
+++ tests/haas/stop_arachni.sh
@@ -4,4 +4,4 @@ HOSTNAME=`hostname`
 ADDRESS=`cat /etc/hosts | grep $HOSTNAME | cut -d "	" -f1` 
 
 echo "Stopping arachni proxy..."
-wget -e use_proxy=yes -e http_proxy=`$ADDRESS`:9595 http://arachni.proxy/shutdown
+wget -e use_proxy=yes -e http_proxy=$ADDRESS:9595 http://arachni.proxy/shutdown
