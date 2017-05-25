6604aa3afc7d09229c851c3295bb116d11cd4d7b
scor <scorlosquet@gmail.com>
2017-05-25 16:05:55 -0400

    clone and en

--- tests/haas/runserver.sh
+++ tests/haas/runserver.sh
@@ -6,4 +6,12 @@ blt ci:setup
 
 echo "Starting Drupal server... "
 cd docroot
+pwd
+cd modules
+git clone git@github.com:CashWilliams/drupen.git
+cd ..
+
 drush runserver `hostname`:8888 &
+
+drush en -y drupen
+drush route-list
