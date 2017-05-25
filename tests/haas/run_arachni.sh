e512c8afa4fb4855dc4d2b0aa036508aa63bc335
Mariano Asselborn <marianoasselborn@gmail.com>
2017-05-25 10:24:46 -0400

    curl

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Scanning with arachni..."
-./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:0:15
+# ./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:0:15
+curl http://`hostname`:8888
