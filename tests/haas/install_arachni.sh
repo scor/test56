27857b2207d51011ed6554d2fc2e8bf02e98ac78
Mariano Asselborn <marianoasselborn@gmail.com>
2017-05-25 09:32:16 -0400

    testz

---
+++ tests/haas/install_arachni.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget -qO- https://github.com/Arachni/arachni/releases/download/v1.5.1/arachni-1.5.1-0.5.12-linux-x86_64.tar.gz | tar xvz -C tests/haas/
+mv tests/haas/arachni* tests/haas/arachni
