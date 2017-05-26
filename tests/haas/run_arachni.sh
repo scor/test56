107dd9df916372840a9ebc87eb2387ea20c5d07f
Cash Williams <CashWilliams@gmail.com>
2017-05-25 21:00:10 -0400

    use wait command

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -7,4 +7,17 @@ echo "Binding proxy on $ADDRESS"
 echo "Scanning with arachni..."
  ./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:10:00 --checks=*,-file_inclusion,-ldap_injection,-no_sql_injection*,-path_traversal,-backdoors,-backup_directories,-backup_files,-common_admin_interfaces,-common_directories,-common_files,-directory_listing,-captcha,-cvs_svn_users,-emails,-private_ip,-ssn,-localstart_asp,-webdav --scope-exclude-pattern '.*\.css' --scope-exclude-pattern '\/devel\/.*' --scope-exclude-pattern '\/sites\/.*\/files\/.*' --scope-exclude-pattern 'user\/logout' --scope-auto-redundant=3 --audit-links --audit-forms --platforms 'linux,sql,mysql,apache,php,symfony' --plugin "proxy:port=9595,bind_address=$ADDRESS"  --scope-extend-paths /mnt/tmp/local.prod/source/tests/haas/drupal_route_list_lite.txt &
 
-sleep 60
+ARACHNIPID=$!
+echo "Arachni PID: $ARACHNIPID"
+
+# we should run behat tests here
+sleep 45
+
+wget -e use_proxy=yes -e http_proxy=$ADDRESS:9595 http://arachni.proxy/shutdown
+
+wait $ARACHNIPID
+
+echo "Arachni completed..."
+
+echo "Killing drush server..."
+pkill -f runserver
