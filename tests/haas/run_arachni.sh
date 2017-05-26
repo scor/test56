0705065c3c05271b478b3dc98ee710f1d96236a5
scor <scorlosquet@gmail.com>
2017-05-26 08:14:46 -0400

    d

--- tests/haas/run_arachni.sh
+++ tests/haas/run_arachni.sh
@@ -5,7 +5,7 @@ ADDRESS=`cat /etc/hosts | grep $HOSTNAME | cut -d "	" -f1`
 echo "Binding proxy on $ADDRESS"
 
 echo "Scanning with arachni..."
-./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:50:00 --checks=csrf,xss*,sql*,code_injection* --scope-exclude-pattern '.*\.(css|js).*' --scope-exclude-pattern '\/devel\/.*' --scope-exclude-pattern '\/sites\/.*\/files\/.*' --scope-exclude-pattern 'user\/logout' --scope-auto-redundant=3 --audit-links --audit-forms --platforms 'linux,sql,mysql,apache,php,symfony' --plugin "proxy:port=9595,bind_address=$ADDRESS"  --scope-extend-paths /mnt/tmp/local.prod/source/tests/haas/drupal_route_list_lite.txt &
+./tests/haas/arachni/bin/arachni http://`hostname`:8888 --timeout 0:30:00 --checks=csrf,xss*,sql*,code_injection* --scope-exclude-pattern '.*\.(css|js).*' --scope-exclude-pattern '\/devel\/.*' --scope-exclude-pattern '\/sites\/.*\/files\/.*' --scope-exclude-pattern 'user\/logout' --scope-auto-redundant=3 --audit-links --audit-forms --platforms 'linux,sql,mysql,apache,php,symfony' --plugin "proxy:port=9595,bind_address=$ADDRESS"  --scope-extend-paths /mnt/tmp/local.prod/source/tests/haas/drupal_route_list_lite.txt &
 
 ARACHNIPID=$!
 echo "Arachni PID: $ARACHNIPID"
